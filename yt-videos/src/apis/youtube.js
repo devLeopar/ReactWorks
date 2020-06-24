367c7753d5483c703449d322c73f73f0482f4be0
devleopar <oktarkara@gmail.com>
2020-06-24 20:28:57 +0300

    youtube-embedded-REACT-finished

--- yt-videos/src/apis/youtube.js
+++ yt-videos/src/apis/youtube.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
+
+const KEY = 'AIzaSyCQ0FdfNgZ97hoR1r1MrDT4P2t_wuO4n5A';
+
 export default axios.create({
    baseURL: 'https://www.googleapis.com/youtube/v3',
+   params:{
+   part: 'snippet',
+   type: 'video',
+   maxResults: 5,
+   key: KEY
+    }
 });
